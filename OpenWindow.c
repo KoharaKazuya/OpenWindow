7b5957ac55b01662214365fbd73a23d814fd09c8
KoharaKazuya <reflect.results@gmail.com>
2013-11-26 21:36:25 +0900

    必要最小限の引数でも動作するように

--- OpenWindow.c
+++ OpenWindow.c
@@ -7,26 +7,26 @@
 // argv[3~] : Program Arguments
 int main(int argc, char* argv[])
 {
-    if (argc < ARGNUM)
-        return 1;
-
-    // 実行するプログラムの引数をまとめる
-    char args[1000] = {0};
-    int i;
-    for (i = ARGNUM; i < argc; ++i) {
-        strcat(args, "\"");
-        strcat(args, argv[i]);
-        strcat(args, "\" ");
-    }
-
     HWND hWin = FindWindow(argv[1], NULL);
     if (hWin) {
         SetForegroundWindow(hWin);
     } else {
+        // 引数の数をチェック
+        if (argc < ARGNUM)
+            return 1;
+        // 実行環境
         char path[255];
         GetCurrentDirectory(255, path);
+        // 実行するプログラムの引数をまとめる
+        char args[1000] = {0};
+        int i;
+        for (i = ARGNUM; i < argc; ++i) {
+            strcat(args, "\"");
+            strcat(args, argv[i]);
+            strcat(args, "\" ");
+        }
+
         ShellExecute(NULL, "open", argv[2], args, path, SW_SHOWDEFAULT);
     }
-
     return 0;
 }
